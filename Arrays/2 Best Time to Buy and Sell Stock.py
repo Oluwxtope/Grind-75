7f405cf7ad14ec0664941ce8e04dc915a0b72e8f
Emmanuel <oluwatopealofe@gmail.com>
2022-06-06 17:38:00 -0400

    cleaned up buy and sell stock solution

--- Arrays/2 Best Time to Buy and Sell Stock.py
+++ Arrays/2 Best Time to Buy and Sell Stock.py
@@ -3,7 +3,6 @@ class Solution:
         start = 0
         end = 1
         max_profit = 0
-        curr_profit = 0
         length = len(prices)
         while end < length:
             if prices[end] < prices[start]:
@@ -11,7 +10,6 @@ class Solution:
                 end = start + 1
             else:
                 curr_profit = prices[end] - prices[start]
-                if curr_profit > max_profit:
-                    max_profit = curr_profit
+                max_profit = max(curr_profit, max_profit)
                 end += 1
         return max_profit
